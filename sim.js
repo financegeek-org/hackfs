283bd6750e7c65090b5117015561fdbdb59800ac
financegeek-org <nlai@rediffmail.com>
2024-06-01 19:27:43 -0700

    add local sim

---
+++ sim.js
@@ -0,0 +1,25 @@
+const query = `What is the world like?`;
+const key = process.env.HUGGING_FACE_API_KEY;;
+
+async function main() {
+  const payload = {
+    "inputs": query,
+  };
+  const response = await fetch(
+    "https://api-inference.huggingface.co/models/gpt2",
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + key,
+      },
+      method: "POST",
+      body: JSON.stringify(payload),
+    }
+  );
+
+  console.log(response);
+  const result = await response.json();
+  console.log(result);
+}
+
+main();
